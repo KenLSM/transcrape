f7fabb649aa218dd54e69b20aef59b234caa1a9c
kenlsm <kenlsm@u.nus.edu>
2016-06-11 19:09:34 -0700

    add scraping of forum threads

--- scrapie/scrapie/spiders/cs_spider.py
+++ scrapie/scrapie/spiders/cs_spider.py
@@ -1,18 +1,26 @@
 import scrapy
+from urllib.parse import urljoin
+
 
 class CSSpider(scrapy.Spider):
     name = "club_house_spider"
     allowed_domains = ["clubhouse.com"]
     start_urls = [
-	"clubhouse.com/forums"  
-	# not properly set. 
-	# Will supply the initial links using another scraper.
-        # this scraper will search within thread?
-	# some other scraper will supply links + handle paignation
-	# "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+        "http://www.clubsnap.com/forums/forumdisplay.php?f=102",
+        "http://www.clubsnap.com/forums/forumdisplay.php?f=104",
     ]
 
+    thread_path = "//*[@class='threadtitle']/a[contains(@class, 'title')]"
+    domain = "http://www.clubsnap.com/forums/"
+
     def parse(self, response):
-        filename = response.url.split("/")[-2] + '.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        thread_urls = []
+        for thread in response.xpath(self.thread_path):
+            urls = [
+                urljoin(self.domain, rel_link)
+                for rel_link in thread.xpath("@href").extract()
+            ]
+
+            thread_urls.extend(urls)
+
+        print(thread_urls)
